c3480dbdd049d53d7b03afaef4de1757f6b9e24c
youngtitanium <youngtitanium.l@gmail.com>
2024-04-25 17:38:54 +0300

    ⏫ 1.0.2 → 1.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="addon-system",
-    version="1.0.2",
+    version="1.0.3",
     packages=[
         "addon_system",
         "addon_system.addon",
