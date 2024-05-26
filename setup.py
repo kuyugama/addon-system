9914986f5620b51e202ac2034b7087bcc35349a7
kuyugama <mail.kuyugama@gmail.com>
2024-05-26 14:38:38 +0300

    ⏫ 1.2.0 → 1.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="addon-system",
-    version="1.2.0",
+    version="1.2.1",
     packages=[
         "addon_system",
         "addon_system.addon",
