d28db9a861c312ed1abbcece97631cb9b7d70948
kuyugama <mail.kuyugama@gmail.com>
2024-06-26 14:43:25 +0300

    ✨Update: Optional dependency version

--- setup.py
+++ setup.py
@@ -27,5 +27,5 @@ setup(
     long_description_content_type="text/markdown",
     classifiers=["Programming Language :: Python :: 3"],
     python_requires=">=3.9",
-    extras_require={"baked": ["pybaked==0.0.10"]},
+    extras_require={"baked": ["pybaked==0.0.11"]},
 )
